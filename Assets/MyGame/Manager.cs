cfe1c54473e2d7530c4e89112b10b984af88ba9d
TobiasNiedermueller <114598146+TobiasNiedermueller@users.noreply.github.com>
2023-09-27 10:38:20 +0200

    1. Commit

---
+++ Assets/MyGame/Manager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public int zahl1;
+    public GameObject eingabe;
+    public Text test;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Plus()
+    {
+        //zahl1 = eingabe.GetComponent<InputField>().text;
+    }
+
+}
